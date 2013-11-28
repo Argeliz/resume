c4aa0406e99c0a2d543d412dbdceba51ff9bb433
Argeliz <cesarargeliz@gmail.com>
2013-11-28 05:05:13 -0800

    Agregada navegacion por secciones

--- js/default.js
+++ js/default.js
@@ -24,5 +24,43 @@ $(document).ready(function(){
 
   // Other initiations go here within the document ready...
 
+  // ANIMAR ELEMENTOS DE NAVEGACIÓN AL PASAR POR LAS SECCIONES
+		$('section').waypoint(function(direction) {
+          var me = $(this); //added
+		  thisId = $(this).attr('id');
+		  $('ul li').each(function(){
+		    var secondaryID = $(this).attr('class');
+		    if  ( secondaryID == thisId )
+		        {
+		        	$('ul li').removeClass('active');
+                    
+                    //added
+                    if(direction==='up'){
+                        me = $(this).prev();
+                    }
+                    
+                    //added
+                    if(!me.length){
+                        me = $(this);
+                    }
+                    
+		            $(this).addClass('active');
+		        }
+			});
+	},{offset: '0'});	
+});	
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 500);
+            return false;
+        }
+    }
 
 }); // end document ready
